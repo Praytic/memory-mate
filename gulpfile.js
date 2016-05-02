70ecce6c413288fccbd6afbc3cfa1f26568f1486
Praytic <chernogorovvm@gmail.com>
2016-05-02 11:31:35 +0300

    Configure gulpfile for clean-up

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var minifyCss     = require('gulp-minify-css');
 var rename        = require('gulp-rename');
 var browserSync   = require('browser-sync');
 var jade          = require('gulp-jade');
+var clean         = require('gulp-clean');
 
 var src = './app/';
 var dist = './www/';
@@ -11,19 +12,27 @@ var reload = browserSync.reload;
 
 var paths = {
   sass:         [src + 'libs/materialize-src/sass/**/*.scss', src + 'styles/**/*.scss'],
-  jade:         [src + 'pages/content/**/*.jade', src + 'pages/deck/**/*.jade', src + 'index.jade'],
+  jade:         [src + 'pages/content/**/*.jade', src + 'pages/deck/**/*.jade', src + 'pages/components/**/*.jade', src + 'index.jade'],
   css:          dist + 'css/',
   img:          src +'img/',
   maincss:      src + 'styles/main.scss',
   mainjs:       dist + 'js/main.js',
-  js:           [src + 'js/**/*.js', src + 'libs/materialize-src/js/bin/materialize.js']
+  js:           [src + 'js/**/*.js', src + 'libs/materialize-src/js/bin/materialize.js'],
+  clean:        [dist + 'pages/components']
 };
 
+gulp.task('clean-up', ['templates'], function () {
+  return gulp.src(paths.clean, {read: false})
+    .pipe(clean());
+});
+
 gulp.task('templates', function () {
-  return gulp.src(paths.jade)
+  return gulp.src(paths.jade, {base: src})
     .pipe(jade())
     .pipe(gulp.dest(dist))
 });
+
+gulp.task('jade', ['templates', 'clean-up']);
 
 gulp.task('sass', function (done) {
   gulp.src([paths.maincss])
@@ -41,7 +50,7 @@ gulp.task('sass', function (done) {
 
 gulp.task('watch', function () {
   gulp.watch(paths.sass, ['sass'], reload);
-  gulp.watch(paths.jade, ['templates'], reload);
+  gulp.watch(paths.jade, ['jade'], reload);
 });
 
 gulp.task('move', function() {
@@ -49,6 +58,6 @@ gulp.task('move', function() {
     .pipe(gulp.dest(dist + 'js/'));
 });
 
-gulp.task('default', ['sass', 'templates', 'move', 'watch'], function () {
+gulp.task('default', ['sass', 'jade', 'move', 'watch'], function () {
   browserSync({server: dist});
 });
